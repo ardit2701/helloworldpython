45aeda9f16229cbbfdc84cf260ee3dcc2e148eed
ardit2701 <ardit.raseni@gmail.com>
2023-12-30 16:04:50 +0100

    f

---
+++ python.py
@@ -0,0 +1,11 @@
+print("Hello World")
+name = "Ardit"
+print("Hello", name)
+print("Hello" + name)
+num = 2
+print("Hello",num)
+print("Hello" + str(num))
+fave_food1 = "lamp"
+fave_food2 = "pizza"
+print("I love to eat {} and {}. ".format(fave_food1,fave_food2) ) 
+print(f"I love to eat {fave_food1} and {fave_food2}." )
